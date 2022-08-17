0fc40ee9f05da1935121856fd371fd0a5e6c9a7e
uskelenov <uskelenovdeal@gmail.com>
2022-08-17 17:47:47 +0300

    test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
